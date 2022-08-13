71fda7e9bcc9a00e83ac51a3f4d77da8fc171409
curso04 <curso04@cic.es>
2022-08-13 10:37:29 +0200

    Refactor de excepciones

--- src/main/java/es/cic/grupo09/grupo09ejerc009/exception/SesionException.java
+++ src/main/java/es/cic/grupo09/grupo09ejerc009/exception/SesionException.java
@@ -1,4 +1,4 @@
-package es.cic.grupo09.grupo09ejerc009.service;
+package es.cic.grupo09.grupo09ejerc009.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;

965662af2572d4c0a06219ca7886ec0e5fcda586
curso04 <curso04@cic.es>
2022-08-13 10:39:07 +0200

    Añadido de servicios base

--- src/main/java/es/cic/grupo09/grupo09ejerc009/service/SesionService.java
+++ src/main/java/es/cic/grupo09/grupo09ejerc009/service/SesionService.java
@@ -5,11 +5,11 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import es.cic.grupo09.grupo09ejerc009.repository.VentaRepository;
+import es.cic.grupo09.grupo09ejerc009.repository.SesionRepository;
 
 @Service
 @Transactional
-public class VentaService {
+public class SesionService {
 	@Autowired
-	private VentaRepository ventaRepository;
+	private SesionRepository sesionRepository;
 }
